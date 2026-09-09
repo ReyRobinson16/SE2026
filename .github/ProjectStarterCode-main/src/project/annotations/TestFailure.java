9ea66c28b20a177c362624098a252f7993d60873
Rrobinson16 <robinsor15@newpaltz.edu>
2026-09-08 21:40:39 -0400

    test

--- .github/ProjectStarterCode-main/src/project/annotations/TestFailure.java
+++ .github/ProjectStarterCode-main/src/project/annotations/TestFailure.java
@@ -1,7 +1,7 @@
-package project;
+package project.annotations;
 
 public class TestFailure {
-    public void badMethod(){
-        int x = 5 // Missing semicolon fails Gradle; bad spacing fails Checkstyle
+    public void badMethod() {
+        int x = 5;
     }
 }
