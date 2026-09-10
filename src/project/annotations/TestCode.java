dffc2155b52c3c881d4056ead3d8ce610887183f
Rrobinson16 <robinsor15@newpaltz.edu>
2026-09-10 12:17:12 -0400

    Create TestCode.java

---
+++ src/project/annotations/TestCode.java
@@ -0,0 +1,5 @@
+public class TestCode {
+    public void brokenMethod() {
+        System.out.println("This is an intentionally extremely long line of code designed to trigger a Checkstyle failure because it easily exceeds standard line length limits")
+    }
+}
