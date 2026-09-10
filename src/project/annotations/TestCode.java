005c772d3d467bf9ad3c121d4a36fd9b5d162b25
Rrobinson16 <robinsor15@newpaltz.edu>
2026-09-10 13:50:08 -0400

    Update TestCode.java

--- src/project/annotations/TestCode.java
+++ src/project/annotations/TestCode.java
@@ -2,6 +2,6 @@ package project.annotations;
 
 public class TestCode {
     public void validMethod() {
-        System.out.println("Valid code.")
+        System.out.println("Valid code.");
     }
 }
