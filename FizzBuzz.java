04bc4ed8b3cec29bdd48e6d4fe9412ce6d86aeea
Rrobinson16 <robinsor15@newpaltz.edu>
2026-09-10 20:33:54 -0400

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,13 +1,12 @@
 public class FizzBuzz {
     public static void main(String[] args) {
-        // Bug 1: Loop should be 1 to 100 inclusive
-        for (int i = 0; i < 100; i++) {
-            // Bug 2: Missing mod 15 check
-            if (i % 3 == 0) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
                 System.out.println("Fizz");
             } else if (i % 5 == 0) {
-                // Bug 3: String spelling typo
-                System.out.println("Buzzz");
+                System.out.println("Buzz");
             } else {
                 System.out.println(i);
             }
