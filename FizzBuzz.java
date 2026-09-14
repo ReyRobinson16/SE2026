d0da344edd02dc34a675a35a791e52940a7ea1bd
Rrobinson16 <robinsor15@newpaltz.edu>
2026-09-14 11:38:27 -0400

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -8,7 +8,7 @@ public class FizzBuzz {
             } else if (i % 5 == 0) {
                 System.out.println("Buzz");
             } else {
-                System.out.println(i)
+                System.out.println(i);
             }
         }
     }
